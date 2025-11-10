9f266cefb58d58ede08218151738298447ab5e72
atmik-prodt <atmiktumane72@gmail.com>
2025-11-10 15:47:37 +0530

    Initial commit - installations

---
+++ src/main/java/com/droidchat/DroidChat/DroidChatApplication.java
@@ -0,0 +1,13 @@
+package com.droidchat.DroidChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroidChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroidChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/droidchat/DroidChat/DroidChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.droidchat.DroidChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroidChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
